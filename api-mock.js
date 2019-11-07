2ac5909fe138217b2718f89da40bbb8f083ba8a5
Frode Hansen <frodehansen@Frodes-MacBook-Pro.local>
2019-11-07 15:54:15 +0100

    Init repo

---
+++ api-mock.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const Busboy = require("busboy");
+
+const server = express();
+
+server.use((req, res, next) => {
+  const allowedOrigins = [
+    "https://omsorgspengesoknad-mock.nais.oera.no",
+    "http://localhost:8080"
+  ];
+  const requestOrigin = req.headers.origin;
+  if (allowedOrigins.indexOf(requestOrigin) >= 0) {
+    res.set("Access-Control-Allow-Origin", requestOrigin);
+  }
+
+  res.removeHeader("X-Powered-By");
+  res.set("X-Frame-Options", "SAMEORIGIN");
+  res.set("X-XSS-Protection", "1; mode=block");
+  res.set("X-Content-Type-Options", "nosniff");
+  res.set("Access-Control-Allow-Headers", "content-type");
+  res.set("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+const søkerMock = {
+  fornavn: "Test",
+  mellomnavn: undefined,
+  etternavn: "Testesen",
+  fodselsnummer: "12345123456",
+  myndig: true
+};
+
+const barnMock = {
+  barn: [
+    {
+      fodselsdato: "1990-01-01",
+      fornavn: "Barn",
+      mellomnavn: "Barne",
+      etternavn: "Barnesen",
+      aktoer_id: "1"
+    },
+    {
+      fodselsdato: "1990-01-02",
+      fornavn: "Mock",
+      etternavn: "Mocknes",
+      aktoer_id: "2"
+    }
+  ]
+};
+
+const arbeidsgivereMock = {
+  organisasjoner: [
+    { navn: "Arbeids- og velferdsetaten", organisasjonsnummer: "123451234" },
+    {
+      navn: "Arbeids- og sosialdepartementet",
+      organisasjonsnummer: "123451235"
+    }
+  ]
+};
+
+const startServer = () => {
+  const port = process.env.PORT || 8082;
+
+  server.get("/health/isAlive", (req, res) => res.sendStatus(200));
+  server.get("/health/isReady", (req, res) => res.sendStatus(200));
+
+  server.get("/arbeidsgiver", (req, res) => {
+    res.send(arbeidsgivereMock);
+  });
+
+  server.get("/soker", (req, res) => {
+    res.send(søkerMock);
+  });
+
+  server.post("/vedlegg", (req, res) => {
+    res.set("Access-Control-Expose-Headers", "Location");
+    res.set("Location", "nav.no");
+    const busboy = new Busboy({ headers: req.headers });
+    busboy.on("finish", () => {
+      res.writeHead(200, { Location: "/vedlegg" });
+      res.end();
+    });
+    req.pipe(busboy);
+  });
+
+  server.get("/barn", (req, res) => res.send(barnMock));
+
+  server.post("/soknad", (req, res) => {
+    res.sendStatus(200);
+  });
+
+  server.listen(port, () => {
+    console.log(`App listening on port: ${port}`);
+  });
+};
+
+startServer();
