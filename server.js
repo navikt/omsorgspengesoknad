b6fe2751a6067e5b8bb8f2c1bfc3aafcb2e1a8eb
Frode Hansen <frode.hansen@bekk.no>
2020-02-04 14:07:38 +0100

    Use PUBLIC_PATH in server.js

--- server.js
+++ server.js
@@ -37,17 +37,10 @@ const renderApp = (decoratorFragments) =>
     });
 
 const startServer = (html) => {
-    server.use(
-        '/familie/sykdom-i-familien/soknad/omsorgspenger/dist/js',
-        express.static(path.resolve(__dirname, 'dist/js'))
-    );
-    server.use(
-        '/familie/sykdom-i-familien/soknad/omsorgspenger/dist/css',
-        express.static(path.resolve(__dirname, 'dist/css'))
-    );
-
-    server.get('/familie/sykdom-i-familien/soknad/omsorgspenger/health/isAlive', (req, res) => res.sendStatus(200));
-    server.get('/familie/sykdom-i-familien/soknad/omsorgspenger/health/isReady', (req, res) => res.sendStatus(200));
+    server.use(`${process.env.PUBLIC_PATH}/dist/js`, express.static(path.resolve(__dirname, 'dist/js')));
+    server.use(`${process.env.PUBLIC_PATH}/dist/css`, express.static(path.resolve(__dirname, 'dist/css')));
+    server.get(`${process.env.PUBLIC_PATH}/health/isAlive`, (req, res) => res.sendStatus(200));
+    server.get(`${process.env.PUBLIC_PATH}/health/isReady`, (req, res) => res.sendStatus(200));
 
     server.get(/^\/(?!.*dist).*$/, (req, res) => {
         res.send(html);
