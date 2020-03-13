b86639c7668ee1be108483039c3e65090b24dc1b
Frode Hansen <frode.hansen@bekk.no>
2020-03-13 20:21:58 +0100

    Update msg

--- src/app/components/corona-warning/CoronaWarning.tsx
+++ src/app/components/corona-warning/CoronaWarning.tsx
@@ -12,12 +12,23 @@ const CoronaWarning: React.FunctionComponent<Props> = (props) => (
             <Box padBottom="m">
                 <Undertittel>Omsorgsdager og koronaviruset</Undertittel>
             </Box>
-            <p style={{ marginTop: 0 }}>
+            <p style={{ marginTop: 0, lineHeight: '1.5rem' }}>
+                Hvis du er arbeidstaker, selvstendig næringsdrivende eller frilanser kan du bruke omsorgsdager{' '}
                 <Lenke href="https://www.nav.no/no/person/innhold-til-person-forside/nyttig-a-vite/stengte-skoler-og-barnehager-gir-rett-til-omsorgspenger">
-                    Yrkesaktive som må være hjemme fra jobb på grunn av stengte skoler og barnehager, kan bruke
-                    omsorgsdager
+                    når du må være hjemme med barn fordi skole eller barnehage har stengt
                 </Lenke>
-                .
+                .<p>Som arbeidstaker er det din arbeidsgiver som skal utbetale omsorgsdager til deg.</p>
+                <p>
+                    Som selvstendig næringsdrivende eller frilanser kan du i{' '}
+                    <Lenke href="https://www.nav.no/familie/sykdom-i-familien/nb/omsorgspenger#Slik-tar-du-ut-omsorgsdager">
+                        noen tilfeller få utbetaling fra NAV
+                    </Lenke>
+                    .
+                </p>
+            </p>
+            <p>
+                Denne søknaden skal du kun bruke hvis du skal søke om ekstra omsorgsdager for kronisk sykt eller
+                funksjonshemmet barn.
             </p>
         </AlertStripeInfo>
     </div>
