28aca212ba3690772bc4aa174fc7c8078571dd5a
Andre Wolden <wolden.andre@gmail.com>
2020-06-18 09:40:32 +0200

    fjerner utkommentert kode#

--- src/app/utils/navigationUtils.ts
+++ src/app/utils/navigationUtils.ts
@@ -7,9 +7,4 @@ const loginUrl = getEnvironmentVariable('LOGIN_URL');
 export const navigateTo = (route: string, history: History) => history.push(route);
 export const navigateToErrorPage = (history: History) => history.push(routeConfig.ERROR_PAGE_ROUTE);
 export const navigateToLoginPage = () => window.location.assign(loginUrl);
-// export const navigateToLoginPage = () => { // To test login flow locally with delay
-//     setTimeout(() => {
-//         window.location.assign(loginUrl);
-//     }, 3000);
-// };
 export const userIsCurrentlyOnErrorPage = () => window.location.pathname === getRouteUrl(routeConfig.ERROR_PAGE_ROUTE);
