feac095112e7c535b3f678f3be129e2c73a07248
Frode Hansen <frode.hansen@bekk.no>
2020-08-05 10:24:55 +0200

    Update texts, add ApplicationMessages

--- src/app/components/intl-provider/IntlProvider.tsx
+++ src/app/components/intl-provider/IntlProvider.tsx
@@ -5,11 +5,10 @@ import '@formatjs/intl-pluralrules/dist/locale-data/nn';
 import '@formatjs/intl-pluralrules/polyfill';
 import bostedUtlandMessages from 'common/forms/bosted-utland/bostedUtlandMessages';
 import { allCommonMessages } from 'common/i18n/allCommonMessages';
-// import MessagesPreview from 'common/dev-utils/intl/messages-preview/MessagesPreview';
 import { Locale } from 'common/types/Locale';
 
-const appBokmålstekster = require('../../i18n/nb.json');
-const appNynorsktekster = require('../../i18n/nn.json');
+export const appBokmålstekster = require('../../i18n/nb.json');
+export const appNynorsktekster = require('../../i18n/nn.json');
 
 const bokmålstekster = { ...allCommonMessages.nb, ...bostedUtlandMessages.nb, ...appBokmålstekster };
 const nynorsktekster = { ...allCommonMessages.nn, ...bostedUtlandMessages.nn, ...appNynorsktekster };
@@ -29,14 +28,6 @@ const IntlProvider = ({ locale, children, onError }: IntlProviderProps) => {
     return (
         <Provider locale={locale} messages={messages} onError={onError}>
             {children}
-            {/* <MessagesPreview
-                title="Søknad ekstra omsorgsdager"
-                showMissingTextSummary={false}
-                messages={{
-                    nb: bokmålstekster,
-                    nn: nynorsktekster
-                }}
-            /> */}
         </Provider>
     );
 };
