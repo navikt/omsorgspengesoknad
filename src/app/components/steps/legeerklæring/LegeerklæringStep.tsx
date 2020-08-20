62a1af547989701c26f48a114048ef8201dbf1f8
Andre Wolden <wolden.andre@gmail.com>
2020-08-20 21:29:00 +0200

    blå gå til søknaden knapp. counsellor panel tekst. ny psg.

--- src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
+++ src/app/components/steps/legeerklæring/LegeerklæringStep.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import Box from 'common/components/box/Box';
-import HelperTextPanel from 'common/components/helper-text-panel/HelperTextPanel';
 import intlHelper from 'common/utils/intlUtils';
 import { StepConfigProps, StepID } from '../../../config/stepConfig';
 import { AppFormField } from '../../../types/OmsorgspengesøknadFormData';
@@ -12,7 +11,6 @@ import FormikFileUploader from '../../formik-file-uploader/FormikFileUploader';
 import FormikStep from '../../formik-step/FormikStep';
 import LegeerklæringFileList from '../../legeerklæring-attachment-list/LegeerklæringAttachmentList';
 import CounsellorPanel from 'common/components/counsellor-panel/CounsellorPanel';
-import FormBlock from 'common/components/form-block/FormBlock';
 import PictureScanningGuide from 'common/components/picture-scanning-guide/PictureScanningGuide';
 import Lenke from 'nav-frontend-lenker';
 
@@ -31,23 +29,19 @@ const LegeerklæringStep = ({ onValidSubmit, formValues }: StepConfigProps) => {
             <Box padBottom="xl">
                 <CounsellorPanel>
                     <p>
-                        Her skal du laste opp bilde av legeerklæringen som legen har skrevet i papirsøknaden. Vær nøye
-                        med at all tekst er med på bildet, inkludert legens signatur. Hvis du ikke har legeerklæring
-                        tilgjengelig når du søker, kan den ettersendes.{' '}
+                        <FormattedMessage id={'step.legeerklaering.counsellorpanel.1'} />
                         <Lenke
                             href="https://www.nav.no/soknader/nb/person/familie/omsorgspenger/NAV%2009-06.05/ettersendelse"
                             target="_blank">
-                            Her får du veiledning til hvordan du ettersender dokumentasjon
+                            <FormattedMessage id={'step.legeerklaering.counsellorpanel.2'} />
                         </Lenke>
                         .
                     </p>
                 </CounsellorPanel>
             </Box>
-            <FormBlock>
-                <HelperTextPanel>
-                    <PictureScanningGuide />
-                </HelperTextPanel>
-            </FormBlock>
+            <Box margin={'l'}>
+                <PictureScanningGuide />
+            </Box>
             <Box margin="l">
                 <FormikFileUploader
                     name={AppFormField.legeerklæring}
