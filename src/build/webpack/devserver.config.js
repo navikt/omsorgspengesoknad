5c99e429508d62650487e7d15fdd87f00a9d988c
Aleksei Poroshin <aleksei.poroshin@nav.no>
2021-09-24 08:35:04 +0200

    Erstatte settings.js med server-kall

--- src/build/webpack/devserver.config.js
+++ src/build/webpack/devserver.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mustacheExpress = require('mustache-express');
-var path = require('path');
+const envSettings = require('../../../envSettings');
 
 /* Start */
 
@@ -9,11 +9,13 @@ const configureDevServer = (decoratorFragments) => ({
         app.engine('html', mustacheExpress());
         app.set('views', `${__dirname}/../../../dist/dev`);
         app.set('view engine', 'mustache');
-        app.get('/dist/js/settings.js', (req, res) => {
-            res.sendFile(path.resolve(`${__dirname}/../../../dist/js/settings.js`));
+        app.get(`${process.env.PUBLIC_PATH}/dist/settings.js`, (req, res) => {
+            res.set('content-type', 'application/javascript');
+            res.send(`${envSettings()}`);
         });
-        app.get(`${process.env.PUBLIC_PATH}/dist/js/settings.js`, (req, res) => {
-            res.sendFile(path.resolve(`${__dirname}/../../../dist/js/settings.js`));
+        app.get('/dist/settings.js', (req, res) => {
+            res.set('content-type', 'application/javascript');
+            res.send(`${envSettings()}`);
         });
         app.get(/^\/(?!.*dist).*$/, (req, res) => {
             res.render('index.html', Object.assign(decoratorFragments));
@@ -23,7 +25,7 @@ const configureDevServer = (decoratorFragments) => ({
     quiet: false,
     noInfo: false,
     stats: 'minimal',
-    publicPath: `${process.env.PUBLIC_PATH}/dist`
+    publicPath: `${process.env.PUBLIC_PATH}/dist`,
 });
 
 module.exports = configureDevServer;
